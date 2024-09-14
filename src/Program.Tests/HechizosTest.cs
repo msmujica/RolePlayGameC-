8cf2a3aac7bafaa41fcaa148e735099ade23f2b3
Martin <mujica0106@gmail.com>
2024-09-14 20:00:11 -0300

    Se agregan clase hechizos con unit test

---
+++ src/Program.Tests/HechizosTest.cs
@@ -0,0 +1,23 @@
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Program.Tests;
+
+[TestClass]
+[TestSubject(typeof(Hechizos))]
+public class HechizosTest
+{
+
+    [TestMethod]
+    public void createHechizo()
+    {
+        string nombre = "Fire Ball";
+        int daño = 10;
+
+        Hechizos fireBall = new Hechizos(nombre, daño);
+        
+        Assert.AreEqual(nombre, fireBall.NombreHechizo);
+        Assert.AreEqual(daño, fireBall.DañoHechizo);
+
+    }
+}
