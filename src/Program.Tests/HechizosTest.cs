186083abb8231010b9486613d0613f2fadaa877a
FacuMartine <facu.martinezduarte@correo.ucu.edu.uy>
2024-09-15 16:29:26 -0300

    a

--- src/Program.Tests/HechizosTest.cs
+++ src/Program.Tests/HechizosTest.cs
@@ -1,46 +1,23 @@
 using JetBrains.Annotations;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Ucu.Poo.Roleplay;
+
+
 [TestClass]
-public class HechizosTests
+[TestSubject(typeof(Hechizos))]
+public class HechizosTest
 {
-    // Prueba que se inicialicen correctamente los valores del hechizo.
+
     [TestMethod]
-    public void TestInicializacionHechizo()
+    public void createHechizo()
     {
-        // Crea un hechizo con nombre y daño específicos
-        Hechizos hechizo = new Hechizos("Bola de Fuego", 50);
+        string nombre = "Fire Ball";
+        int daño = 10;
 
-        // Verifica que el nombre del hechizo sea correcto
-        Assert.AreEqual("Bola de Fuego", hechizo.NombreHechizo);
+        Hechizos fireBall = new Hechizos(nombre, daño);
+        
+        Assert.AreEqual(nombre, fireBall.NombreHechizo);
+        Assert.AreEqual(daño, fireBall.DañoHechizo);
 
-        // Verifica que el daño del hechizo sea correcto
-        Assert.AreEqual(50, hechizo.DañoHechizo);
-    }
-
-    // Prueba la asignación de un nuevo nombre al hechizo.
-    [TestMethod]
-    public void TestAsignarNombreHechizo()
-    {
-        Hechizos hechizo = new Hechizos("Bola de Fuego", 50);
-
-        // Cambia el nombre del hechizo
-        hechizo.NombreHechizo = "Rayo Congelante";
-
-        // Verifica que el nuevo nombre sea correcto
-        Assert.AreEqual("Rayo Congelante", hechizo.NombreHechizo);
-    }
-
-    // Prueba la asignación de un nuevo valor de daño al hechizo.
-    [TestMethod]
-    public void TestAsignarDañoHechizo()
-    {
-        Hechizos hechizo = new Hechizos("Bola de Fuego", 50);
-
-        // Cambia el valor del daño del hechizo
-        hechizo.DañoHechizo = 75;
-
-        // Verifica que el nuevo valor del daño sea correcto
-        Assert.AreEqual(75, hechizo.DañoHechizo);
     }
 }
